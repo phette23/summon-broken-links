c3081696c8fff70bb9438f39d1b7266e95c91546
phette23 <phette23@gmail.com>
2019-10-07 16:47:37 -0700

    clean up example of using authenticated API
    abstract out painful parts of API request to a library
    will rewrite get-query-data using the tools in api.js soon

---
+++ src/api.js
@@ -0,0 +1,73 @@
+const crypto = require('crypto')
+
+const key = require('./auth.json').key
+let accept = 'application/json'
+let accessId = 'cca'
+let host = 'api.summon.serialssolutions.com'
+let path = '/2.0.0/search'
+
+/**
+ * @param {array} q - an array of {key:value} parameter objects like
+ * [ { "s.q": "nietzsche" }, { "s.fvf": "ContentType..." } ]
+ * @returns {string} sorted but NOT URI-encoded query string
+ * s.fvf=ContentType...&s.q=nietzsche
+ */
+function sortQuery(q) {
+    return q.map(o => `${Object.keys(o)[0].toString()}=${Object.values(o)[0].toString()}`).sort().join('&')
+}
+
+/**
+ * makeAuthDigest
+ * https://developers.exlibrisgroup.com/summon/apis/SearchAPI/Authentication/
+ * @param {array} query - an array of {key:value} hashes of Summon search params
+ * [ { "s.q": "nietzsche" }, { "s.fvf": "ContentType,Journals,false" } ]
+ * @param {Date} current date in UTC time e.g. new Date().toUTCString()
+ * since this is also used in the HTTP request headers we need to make it a
+ * parameter rather than just have this function calculate it
+ * @returns {string} auth digest string for use in Authorization HTTP header
+ */
+function makeAuthDigest(query, date) {
+    // https://github.com/summon/summon-api-toolkit/blob/master/node.js/summonapidemo.js
+    let qs = sortQuery(query)
+    let idString = [accept, date, host, path, qs].join('\n') + '\n'
+    let hmac = crypto.createHmac('sha1', key)
+    let hash = hmac.update(idString)
+    return hash.digest('base64')
+}
+
+
+/**
+ * headers
+ * https://developers.exlibrisgroup.com/summon/apis/SearchAPI/Authentication/
+ * @param {array} query - an array of {key:value} hashes of Summon search params
+ * [ { "s.q": "nietzsche" }, { "s.fvf": "ContentType,Journals,false" } ]
+ * @returns {object} key:value pairs representation of HTTP headers
+ * for use in request({ url: url, headers: headers })
+ */
+function headers(query) {
+    let date = new Date().toUTCString()
+    let digest = makeAuthDigest(query, date)
+    let headers = {
+        'Accept': accept,
+        'Authorization': `Summon ${accessId};` + digest,
+        'Host': host,
+        'x-summon-date': date,
+    }
+    return headers
+}
+
+/**
+ * url
+ * https://developers.exlibrisgroup.com/summon/apis/SearchAPI/Authentication/
+ * @param {array} query - an array of {key:value} hashes of Summon search params
+ * [ { "s.q": "nietzsche" }, { "s.fvf": "ContentType,Journals,false" } ]
+ * @returns {string} query string suitable for constructing a Summon API request
+ * URL (sorted but not URI encoded)
+ */
+function url(query) {
+    // we actually don't need the query string to be properly sorted in the URL
+    // but it's easier to use the array=>string method we already wrote
+    return `https://${host}${path}?${sortQuery(query)}`
+}
+
+module.exports = { makeAuthDigest, headers, url }
--- src/with-auth-api.js
+++ src/with-auth-api.js
@@ -1,39 +1,18 @@
 #!/usr/bin/env node
-const crypto = require('crypto')
+const fs = require("fs")
 
 const request = require('request')
 
-const key = require('./auth.json').key
-// https://developers.exlibrisgroup.com/summon/apis/SearchAPI/Authentication/
-// all these parameters are used to construct the auth string
-let accept = 'application/json'
-let date = new Date().toUTCString()
-let host = 'api.summon.serialssolutions.com'
-let path = '/2.0.0/search'
-let query = 's.q=beyond good and evil'
+const api = require("./api.js")
 
-// join the 5 parameters, appending a newline to each (including the last)
-let idString = [accept, date, host, path, query].join('\n') + '\n'
-// https://github.com/summon/summon-api-toolkit/blob/master/node.js/summonapidemo.js
-let hmac = crypto.createHmac('sha1', key)
-let hash = hmac.update(idString)
-let digest = hash.digest('base64')
+// query is an array of key:value param hashes
+let query = [ { "s.q": "beyond good or evil"}, { "s.ho": "t"} ]
 
-let headers = {
-    'Accept': accept,
-    'Authorization': 'Summon cca;' + digest,
-    'Host': host,
-    'x-summon-date': date,
-}
-let opts = {
-    url: `https://${host}${path}?${query}`,
-    headers: headers,
+let options = {
+    url: `${api.url(query)}`,
+    headers: api.headers(query),
 }
 
-function handler (err, response, body) {
-    if (err) throw err
+console.log(options)
 
-    console.log(body)
-}
-
-request.get(opts, handler)
+request(options).pipe(fs.createWriteStream("api.json"))
