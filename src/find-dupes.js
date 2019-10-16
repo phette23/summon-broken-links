1fda0cf4bb2a1de39d1abd068eb12ad0ac63f802
phette23 <phette23@gmail.com>
2019-10-16 14:45:34 -0700

    find dupes - match title matching much less stringent

--- src/find-dupes.js
+++ src/find-dupes.js
@@ -6,20 +6,25 @@ const async = require("async")
 
 const stringify = require("./stringify.js")
 const dir = 'data/results'
+const titleMap = (d) => d.Title[0]
+    .toLowerCase()
+    .replace(/<h>/g, '').replace(/<\/h>/g, '')
+    .replace(/[!#?$%^&*()[\]."'/\\-]/g, '')
+    .trim()
 
 // only does a naive check for identical titles
 function checkDupes(docs) {
     let dupes = []
     while (docs.length > 0) {
         let doc = docs.shift()
-        let title = doc.Title[0]
+        let title = titleMap(doc)
         // title is included in other documents' titles
-        if (docs.map(d => d.Title[0]).includes(title)) {
+        if (docs.map(titleMap).includes(title)) {
             // add this doc to dupes
             dupes.push(doc)
             // iterate & find other matching docs, splicing them off
-            while (docs.map(d => d.Title[0]).indexOf(title) > -1) {
-                let index = docs.map(d => d.Title[0]).indexOf(title)
+            while (docs.map(titleMap).indexOf(title) > -1) {
+                let index = docs.map(titleMap).indexOf(title)
                 dupes.push(docs[index])
                 docs.splice(index, 1)
             }
@@ -42,7 +47,7 @@ fs.readdir(dir, (err, files) => {
         // results is array of arrays
         fs.writeFile('data/duplicates.json', stringify(results.flat()), (err) => {
             if (err) throw err
-            console.log('Wrote potential list of duplicate records to data/duplicates.json.')
+            console.log(`Wrote potential list of ${results.flat().length} duplicate records to data/duplicates.json.`)
         })
     })
 })
