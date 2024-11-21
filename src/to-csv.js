da6c74f7124777f53f59fb22a347a24edd2908fa
phette23 <phette23@gmail.com>
2024-11-21 09:53:38 -0800

    fix: to-csv.js improvements
    - add more fields from Summon API to the CSV
    - make the str() fn stringify booleans as well
    - convert nulls to emptry string

--- src/to-csv.js
+++ src/to-csv.js
@@ -16,23 +16,32 @@ let fields = {
     ID: 'ID',
     MergedId: 'MergedId',
     LinkModel: 'LinkModel',
+    SourceType: 'SourceType',
+    Source: 'SourceID',
+    PublicationDecade: 'PublicationDecade',
+    Scholarly: 'IsScholarly',
+    PeerReviewed: 'IsPeerReviewed',
+    OpenAccess: 'IsOpenAccess',
+    FullTextHit: 'isFullTextHit',
 }
 let fieldNames = Object.keys(fields)
 let linkCheckFields = [
     "destination",
+    "resolves_to_full_text",
     "duplicate",
     "full_text",
     "notes",
-    "resolves_to_full_text",
 ]
 let escape = (s) => s.replace(/"/g,'""')
 
 // flatterns & stringifies arrays while not choking on other input
 // this DOES choke on arrays of objects e.g. PublicationDate_xml, Publisher_xml
+// str(true) => "true", str([1, 2]) => "1, 2", str({}) => ''
 function str(arg) {
     // return comma-separated string, skipping over empty entries
     if (Array.isArray(arg)) return escape(arg.filter(i => i != '').join(', '))
     if (typeof arg === 'string') return escape(arg)
+    if (typeof arg === 'boolean') return arg.toString()
     return ''
 }
 
@@ -41,13 +50,21 @@ fs.readFile(filename, 'utf8', (err, data) => {
     let docs = JSON.parse(data)
     // abstract over analysis vs results files
     if (docs.documents) docs = docs.documents
+
     // print header row
     process.stdout.write(`"${fieldNames.join('","')}"`)
+
+    // if we have link_check data include that in CSV as well
     if (docs[0].link_check) process.stdout.write(`,"${linkCheckFields.join('","')}"`)
     process.stdout.write('\n')
+
     docs.forEach(d => {
         let strings = fieldNames.map(field => str(d[fields[field]]))
-        if (d.link_check) linkCheckFields.forEach(f => strings.push(d["link_check"][f].toString()))
+        if (d.link_check) linkCheckFields.forEach(f => {
+            // convert nulls to empty string
+            if (d["link_check"][f] !== null) return strings.push(d["link_check"][f].toString())
+            return strings.push("")
+        })
         console.log(`"${strings.join('","')}"`)
     })
 })
